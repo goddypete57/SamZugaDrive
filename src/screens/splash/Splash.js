03b52f3ec0ce6665b54735233d434112aa4ba6e5
Vcky4 <okon.victor.u@gmail.com>
2022-12-15 10:27:38 +0100

    fix

--- src/screens/splash/Splash.js
+++ src/screens/splash/Splash.js
@@ -13,7 +13,7 @@ export default Splash = () => {
                         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
                     )
                     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                        setLoading(true);
+                        setLoading(false);
                     } else {
                         BackHandler.exitApp()
                     }
