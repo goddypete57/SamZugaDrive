ca0c41a75b4e7900561f73562518ae0679f4eb62
Vcky4 <okon.victor.u@gmail.com>
2022-12-15 09:36:46 +0100

    some stuff

--- src/screens/auth/OnboardScreen.js
+++ src/screens/auth/OnboardScreen.js
@@ -1,96 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import colors from '../../../assets/colors/colors';
-import GradientText from '../../Component/GradientText';
-import authRoute from '../../navigation/route/authRoute';
-import Button from '../../Component/Button';
 
-export default OnboardingScreen = ({navigation}) => {
+
+export default Onboarding = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerWrapper}>
-       <Text style={styles.header}>
-       Welcome to DSP!
-       </Text>
-        <Text style={styles.subtext}>
-          Our mission is to provide help to{'\n'} endangered persons
-        </Text>
-      </View>
-      <View style={styles.imageWrapper}>
-        <View style={styles.ImageView}>
-          <Image source={require('../../../assets/images/policeImage.png')} />
-        </View>
-      </View>
-      <View>
-        <Button
-          title={'Set up your account'}
-          textColor={colors.white}
-          enabled={true}
-          onPress={()=> {navigation.navigate(authRoute.setUpAccount)}}
-          buttonStyle={styles.Button}
-        />
-        <Button
-          title={'Login'}
-          enabled={true}
-          onPress={()=> {navigation.navigate(authRoute.login)}}
-          textColor={colors.textColor1}
-          buttonStyle={styles.Button2}
-          color={colors.gray2}
-        />
-      </View>
+    <View>
+      <Text>Onboarding Screen</Text>
     </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.white,
-  },
-  header:{
-    color: colors.green3,
-    fontSize: 32,
-    fontFamily: 'Outfit-Medium',
-  },
-  headerWrapper: {
-    marginStart: 24,
-    marginTop: 74,
-  },
-  headerText: {
-    color: colors.deepGreen,
-    fontSize: 32,
-    fontFamily: 'Outfit-Medium',
-  },
-  subtext: {
-    color:  'rgba(42, 83, 76, 0.7)',
-    fontSize: 18,
-    fontFamily: 'Outfit-Regular',
-    marginTop: 5,
-  },
-
-  imageWrapper: {
-    width: 312,
-    height: 277,
-    backgroundColor: colors.gray,
-    alignSelf: 'center',
-    marginTop: 43,
-    borderRadius: 5,
-  },
-  ImageView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 26,
-  },
-  Button: {
-    width: 312,
-    height: 50,
-    alignSelf: 'center',
-    marginTop: 77,
-  },
-  Button2: {
-    width: 312,
-    height: 50,
-    alignSelf: 'center',
-    marginTop: 20,
-  },
-});
+  )
+}
