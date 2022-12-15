3c4110b09baf55008461326257de4fc9e229b39f
Vcky4 <okon.victor.u@gmail.com>
2022-12-15 09:52:31 +0100

    fix

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 export default Login = ({ navigation }) => {
   return (
     <View>
-      <Text>Login Screen</Text>
+      <Text>Home Screen</Text>
     </View>
   )
 }
